a2e1805b8af00f2ef0b05fabad8d9ce459b6e4ca
NamSellsFish <nhatnam0192123@gmail.com>
2023-10-28 01:10:01 +0700

    hoàn thành bản nháp LogIn UI

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -16,6 +16,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+    <head>
+    <meta charSet="UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
+        integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
+        crossOrigin="anonymous" referrerPolicy="no-referrer" />
+    </head>
       <body className={inter.className}>{children}</body>
     </html>
   )
--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,9 +1,15 @@
+'use client';
+import AuthenticationModal from '@/components/AuthenticationModalComponent/AuthenticationModalComponent';
 import Image from 'next/image'
-
 export default function Home() {
-  return (
-    <main>
-      Hello
-    </main>
-  )
+        function handleOpenAuthentication(){
+            const authenticationModal = (document.getElementById("authenticationModal") as HTMLDialogElement);
+            authenticationModal!.showModal();
+        }
+        return (
+            <>
+             <button className="button open-button" onClick={handleOpenAuthentication}>open modal</button>
+             <AuthenticationModal></AuthenticationModal>
+           </>
+        )
 }
