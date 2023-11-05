b2a91f523aadb309064ea805ab6190948918ba2a
cuonglexuan203 <cuonglexuan203@gmail.com>
2023-11-05 12:50:28 +0700

    Add test case for entities

--- server/sneaker-server/src/main/java/com/hcmute/sneakerstore/controllers/TestDB.java
+++ server/sneaker-server/src/main/java/com/hcmute/sneakerstore/controllers/TestDB.java
@@ -1,45 +1,50 @@
-//package com.hcmute.sneakerstore.controllers;
-//
-//import jakarta.persistence.EntityManager;
-//import jakarta.persistence.EntityManagerFactory;
-//import jakarta.persistence.EntityTransaction;
-//import jakarta.persistence.Persistence;
-//import jakarta.servlet.ServletException;
-//import jakarta.servlet.annotation.WebServlet;
-//import jakarta.servlet.http.HttpServlet;
-//import jakarta.servlet.http.HttpServletRequest;
-//import jakarta.servlet.http.HttpServletResponse;
-//import jakarta.transaction.Transaction;
-//
-//import java.io.IOException;
-//
-//import com.hcmute.sneakerstore.business.Test;
-//import com.hcmute.sneakerstore.business.User;
-//
-//@WebServlet("/test")
-//public class TestDB extends HttpServlet {
-//	private static final long serialVersionUID = 1L;
-//
-//	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-//			throws ServletException, IOException {
-//
-//		try (EntityManagerFactory emf = Persistence.createEntityManagerFactory("sneakerdb");
-//				EntityManager em = emf.createEntityManager();) {
-//			User user = new User();
-//			user.setUser_id(1);
-//			user.setFirstname("Cuong");
-//			
-//			EntityTransaction tran = em.getTransaction();
-//			tran.begin();
-//
-//			em.persist(user);
-//			tran.commit();
-//			response.getWriter().println(user.getUser_id() + ": " + user.getFirstname());
-//			Test a = new Test();
-//			
-//		} catch (Exception ex) {
-//
-//		}
-//	}
-//
-//}
+package com.hcmute.sneakerstore.controllers;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.time.LocalDate;
+
+import com.hcmute.sneakerstore.business.Location;
+import com.hcmute.sneakerstore.business.User;
+
+
+@WebServlet("/test")
+public class TestDB extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		
+		try (EntityManagerFactory emf = Persistence.createEntityManagerFactory("sneakerdb");
+				EntityManager em = emf.createEntityManager();) {
+			
+			EntityTransaction tran = em.getTransaction();
+			tran.begin();
+			User user = new User();
+			user.setFirstName("LXC");
+			user.setLastName("Non");
+			user.setGender(true);
+			user.setBirthday(LocalDate.now());
+			user.setPhoneNumber("012");
+			user.setAddress(new Location("VN", "VN", "VN"));
+			em.persist(user);
+			tran.commit();
+			
+			
+		} catch (Exception ex) {
+
+		}
+		response.getWriter().println("Hello world!");
+	}
+
+}
