203c1adfd33dee1439e0663135ab8b193bba7bc4
NamSellsFish <nhatnam0192123@gmail.com>
2023-11-11 22:10:01 +0700

    update haha

--- server/sneaker-server/src/main/java/com/hcmute/sneakerstore/business/Identifiable.java
+++
@@ -1,5 +0,0 @@
-package com.hcmute.sneakerstore.business;
-
-public interface Identifiable {
-	long getId();
-}
--- server/sneaker-server/src/main/java/com/hcmute/sneakerstore/business/enums/DeliveryStatus.java
+++
@@ -1,5 +0,0 @@
-package com.hcmute.sneakerstore.business.enums;
-
-public enum DeliveryStatus {
-	PENDING, IN_TRANSIT, SHIPPED, CANCELLED
-}
