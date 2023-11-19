407eb1c9a4c03142cf8c526a2ce412b196977d68
cuonglexuan203 <cuonglexuan203@gmail.com>
2023-11-20 01:32:36 +0700

    fix the authOptions can not build in production

--- client/src/app/api/auth/[...nextauth]/route.ts
+++ client/src/app/api/auth/[...nextauth]/route.ts
@@ -3,7 +3,7 @@ import GitHubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
 
 
-export const authOptions = {
+const authOptions = {
     providers: [
         GitHubProvider({
             clientId: process.env.GITHUB_ID ?? "",
