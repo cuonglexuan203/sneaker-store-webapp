64eed9ce44b7f65d2c5dc1db25272c3d139bce0c
cuonglexuan203 <cuonglexuan203@gmail.com>
2023-11-20 09:57:35 +0700

    fix the handler ( [...nextauth]/route.ts ) can not build in production

--- client/src/app/api/auth/[...nextauth]/route.ts
+++ client/src/app/api/auth/[...nextauth]/route.ts
@@ -1,9 +1,9 @@
-import NextAuth from "next-auth";
+import NextAuth , {NextAuthOptions} from "next-auth";
 import GitHubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
 
 
-const authOptions = {
+const authOptions: NextAuthOptions = {
     providers: [
         GitHubProvider({
             clientId: process.env.GITHUB_ID ?? "",
@@ -19,6 +19,6 @@ const authOptions = {
     }
 }
 
-export const handler = NextAuth(authOptions);
+const handler = NextAuth(authOptions);
 
 export {handler as GET, handler as POST};
