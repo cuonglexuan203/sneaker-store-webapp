b7e13b89c6be523bbf0f57ab6f2a24b826246c3d
NamSellsFish <nhatnam0192123@gmail.com>
2023-11-25 20:39:37 +0700

    update cart Page

--- client/src/app/(main)/cart/page.tsx
+++ client/src/app/(main)/cart/page.tsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import { promises as fs } from "fs"
+import path from "path"
+import { Metadata } from "next"
 
-const Cart = () => {
-    return (
-        <section>
-            Cart
-        </section>
-    )
+import { LineItemTable } from "../_components/cart_page_components/line-items-table"
+
+export const metadata: Metadata = {
+  title: "Line Items",
+  description: "A shopping cart build using Tanstack Table.",
 }
 
-export default Cart
+// Simulate a database read for line Items.
+async function getLineItems() {
+  const data = await fs.readFile(
+    path.join(process.cwd(), "src/app/(main)/_components/cart_page_components/data/lineItems.json")
+  )
+  return JSON.parse(data.toString())
+}
+
+export default async function CartPage() {
+
+  const lineItems = await getLineItems()
+  return (
+    <>
+          <LineItemTable data={lineItems}/>
+    </>
+  )
+}
+
